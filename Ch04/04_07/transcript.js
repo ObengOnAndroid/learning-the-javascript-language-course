ebb140703a79576b036d02d2640344ee1419ef18
Eryk G <erykg7676@gmail.com>
2024-12-19 23:12:51 +0000

    Elvis operator

--- Ch04/04_07/transcript.js
+++ Ch04/04_07/transcript.js
@@ -9,6 +9,16 @@ var job = animal === "cat" ? "cat herder" : "dog catcher";
 
 // prettier-ignore
 var job = (animal === "cat") ? "cat herder" : "dog catcher";
+var abc = ((12%2)===0)? console.log("even!"):console.log("odd");
+
+function isEven(num){
+  if (num%2 ===0){
+    return true
+  }
+  else{
+    return false
+  }
+}
 
 // More info:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling#if...else_statement
